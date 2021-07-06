b6113c7b5decdb3ee70ab6c2567233376c7cc3ea
vivostanoev <vivostanoev@gmail.com>
2021-07-07 00:24:08 +0300

    ccs transition

--- src/Menu/FoodGrid.js
+++ src/Menu/FoodGrid.js
@@ -20,11 +20,17 @@ export const Food = styled(Title)`
     background-image: ${({ img }) => `url(${img});`}
     background-position: center;
     background-size: cover;
+    margin-top:5px;
+    transition-property: box-shadow margin-top filter;
+    transition-duration: .1s;
     filter: contrast(75%);
     border-radius: 15px;
     box-shadow: 0px 0px 3px 1px grey;
     &:hover {
         cursor: pointer;
-        opacity: 0.7;
+        filter: contrast(100%);
+        margin-top:0px;
+        margin-bottom:5px;
+        box-shadow: 0px 5px 5px 1px grey;
     }
 `
