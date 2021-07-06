79605854b4404bf1a2c409870e88cc2e7b7b0998
vivostanoev <vivostanoev@gmail.com>
2021-07-06 11:05:48 +0300

    add google fonts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`
+
 function App() {
   return (
-    <div>Hello Pizza</div>
+    <>
+        <h1>Pizza app</h1>
+        <GlobalStyle/>
+        <div>Hello Pizza</div>
+    </>
   );
 }
 export default App;
