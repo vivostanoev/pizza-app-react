3727e590608e6361ff181f0d4f1e5b01120177dd
vivostanoev <vivostanoev@gmail.com>
2021-07-07 09:52:11 +0300

    refactoring globalStyle

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import { createGlobalStyle } from 'styled-components'
+import { GlobalStyle } from './Styles/GlobalStyle.js'
 import { Navbar } from "./Navbar/Navbar.js"
 import { Banner } from "./Banner/Banner.js"
 import { Menu } from "./Menu/Menu.js"
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin : 0;
-    font-family: 'Open Sans', sans-serif;
-  }
-  h1, h2, h3 {
-    font-family: 'Righteous', cursive;
-  }
-`
 
 function App() {
   return (
