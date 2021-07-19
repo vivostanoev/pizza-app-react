a4678bed6c5697051d967ec067a2621e7e27a9d6
vivostanoev <vivostanoev@gmail.com>
2021-07-20 00:29:58 +0300

    add total price/ tax

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -39,6 +39,14 @@ const OrderItem = styled.div`
 `;
 
 export function Order({orders}) {
+
+    const subtotal = orders.reduce((total, order) => {
+        return total += getPrice(order);
+    },0);
+
+    const tax = subtotal * 0.10;
+    const total = tax + subtotal;
+
     return (<OrderStyled>
             {orders.length == 0 ? (<OrderContent> Your order`s empty </OrderContent>)
             : (<OrderContent>
@@ -53,6 +61,23 @@ export function Order({orders}) {
                         </OrderItem>
                      </OrderContainer>
                     ))}
+                    <OrderContainer>
+                        <OrderItem>
+                            <div/>
+                            <div>Sub-Total</div>
+                            <div>{formatPrice(subtotal)}</div>
+                        </OrderItem>
+                        <OrderItem>
+                            <div/>
+                            <div>Tax</div>
+                            <div>{formatPrice(tax)}</div>
+                        </OrderItem>
+                        <OrderItem>
+                            <div/>
+                            <div>Total</div>
+                            <div>{formatPrice(total)}</div>
+                        </OrderItem>
+                    </OrderContainer>
                 </OrderContent>)}
             <DialogFooter>
                 <ConfirmButton>Checkout</ConfirmButton>
